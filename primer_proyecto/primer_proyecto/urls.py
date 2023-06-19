7401fd821901e693532e6557b4f7870b9f65eaf2
Javier <95302543+el-javier@users.noreply.github.com>
2023-06-19 10:29:13 -0400

    Add files via upload

--- primer_proyecto/primer_proyecto/urls.py
+++ primer_proyecto/primer_proyecto/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mi_app.views import landing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing),
 ]
